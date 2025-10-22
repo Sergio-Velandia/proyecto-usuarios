e518cdec132e4760a4f8dd9e73f994401b231296
Sergio Velandia <sergio.velandiar.z@gmail.com>
2025-10-22 16:56:14 +0000

    feat: initial project scaffolding with React and Express setup

--- proyecto-usuarios/backend/server.js
+++ proyecto-usuarios/backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const usuariosRoutes = require('./routes/usuarios');
 
 const app = express();
-const PORT = 5001;
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
